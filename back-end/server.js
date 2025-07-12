be92e937eefc01f034e4c96818ba0a40f86fe890
pratik0809 <pratikkbirla@gmail.com>
2025-07-12 22:16:39 +0530

    10th commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,23 +1,23 @@
 import express from 'express'
 
 import cors from 'cors'
+import dotenv from 'dotenv'
 import 'dotenv/config'
 
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
-import dotenv from 'dotenv'
 import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
 
 
 
 // app config
+dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 connectDB()
 connectCloudinary()
-dotenv.config()
 
 
 // middlewares 
