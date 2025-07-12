32f1fec28057cd64299ba97e41b1a79be47203b9
pratik0809 <pratikkbirla@gmail.com>
2025-07-12 19:38:32 +0530

    5th commit

--- back-end/server.js
+++ back-end/server.js
@@ -23,7 +23,7 @@ dotenv.config()
 // middlewares 
 app.use(express.json())
 app.use(cors({
-  origin: ['https://prescripto-admin-ru62.onrender.com', 'https://prescripto-frontend-XYZ.onrender.com'], // add all frontend origins
+  origin: ['https://prescripto-admin-ru62.onrender.com', 'https://prescripto-front-end-5a0i.onrender.com'], // add all frontend origins
   credentials: true // if you're using cookies or auth headers
 }));
 
