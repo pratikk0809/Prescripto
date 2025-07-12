03788f0ac04706066ea14e21be7765bbd4c3a52a
pratik0809 <pratikkbirla@gmail.com>
2025-07-12 19:02:56 +0530

    3rd commit

--- front-end/src/context/AppContext.jsx
+++ front-end/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://prescripto-backend-074r.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
